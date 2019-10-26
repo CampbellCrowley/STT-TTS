4173cad2c326c1e1e6696fcad8750cb6d749c28e
Campbell Crowley <github@campbellcrowley.com>
2019-10-26 15:23:04 -0700

    Update README.md, add comments

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # STT-TTS
-By default, uses default audio input and output for recording and playback.  
+By default, uses default audio input and output devices for recording and playback.  
 
-Pressing Alt + NumPadPlus will process the last 3 seconds of audio from the
-microphone.  
+Pressing `F2` will process the last 2 seconds of audio from the microphone.  
+Pressing `Meta + u` will play the last 2 seconds of audio from the microphone
+directly to the output.
 
 Text sent into stdin will be sent directly to tts.
 
--- index.js
+++ index.js
@@ -10,26 +10,41 @@ const fs = require('fs');
 // 1 is display audio heartbeat and keyboard triggers.
 // 2 is display keyboard keycodes.
 // STDOUT writing can cause blocking on some systems.
-const DEBUG = 1;
+const DEBUG = 0;
 
+// Audio sample rate for the entire system. Expected to match device settings.
 const sampleRate = 44100;
+// Audio sample bit format. Must match `startup.wav`.
 const sampleFormat = portAudio.SampleFormat16Bit;
+// Number of milliseconds long each audio chunk will be.
 const deltaSample = 50;
+// Total number of milliseconds of audio to buffer.
 const historyLength = 2000;
 
+// Path to the Google API credentials file.
 process.env.GOOGLE_APPLICATION_CREDENTIALS = './gApiCredentials.json';
+
+// List all devices to log so user knows available options.
 const devices = portAudio.getDevices();
 function nameFilter(name) {
   return name.replace(/\n/g, '').replace(/\s/g, ' ').replace('%0 ;', ' ');
 }
 console.log(devices.map((el) => `${el.id}: ${nameFilter(el.name)}`).join('\n'));
 
+// Used if DEBUG == 1, to reduce times to write to stdout when unnecessary.
 let lastRoll;
+// Timeout after audio input has died to kill all input and restart from scratch
+// as an attempt to recover from a failure.
 let audioTimeout = null;
+// Is a request for STT-TTS queued. If true, after the next chunk of audio is
+// received, the whole buffer is sent to the API for processing.
 let queued = false;
 
+// The audio input device object reference.
 let input;
+// Setup the audio input device.
 function setupInput() {
+  // ID of the audio device to use.
   const iId = portAudio.getDevices()
                 .find((el) => el.name.indexOf('CABLE Output') > -1)
                 .id;
@@ -47,8 +62,11 @@ function setupInput() {
   input.start();
 }
 
+// Audio output device object reference.
 let output;
+// Setup the audio output device.
 function setupOutput() {
+  // ID of the output device.
   const oId = portAudio.getDevices()
                 .find((el) => el.name.indexOf('VoiceMeeter VAIO3 Input') > -1)
                 .id;
@@ -67,9 +85,13 @@ function setupOutput() {
   });
   output.start();
 
+  // Play a sound in order to prepend the data with a valid WAV header prior to
+  // streaming our data. `startup.wav` must match the format of received audio
+  // from the TTS API.
   fs.createReadStream('./startup.wav').on('data', (d) => output.write(d));
 }
 
+// Default TTS request settings.
 const ttsRequest = {
   input: {text: 'Hello World'},
   voice: {
@@ -84,7 +106,10 @@ const ttsRequest = {
     speakingRate: 1.0,
   },
 };
+// Directory to store cached audio files so subsequent requests for the same text
+// do not require accessing the API.
 const cacheDir = `./cache/${ttsRequest.voice.name}/`;
+// Default STT request settings.
 const sttRequest = {
   audio: {content: null},
   config: {
@@ -94,11 +119,18 @@ const sttRequest = {
   },
 };
 
+// Buffer of the last few seconds of audio from the audio input.
 const audioBuffer = [];
 
 const ttsClient = new tts.TextToSpeechClient();
 const sttClient = new stt.SpeechClient();
 
+/**
+ * Convert the given text into speech. Response is played through the output
+ * device.
+ *
+ * @param {string} text The text to convert to speech.
+ */
 function toSpeech(text) {
   const filename = `${cacheDir}${encodeURIComponent(text)}.wav`;
   if (fs.existsSync(filename)) {
@@ -133,6 +165,10 @@ function toSpeech(text) {
   });
 }
 
+/**
+ * Trigger the STT-TTS. If audio input data is available, it will be sent to the
+ * STT API, and the response text is then sent to {@link toSpeech}.
+ */
 function trigger() {
   if (audioBuffer.length * deltaSample < 200) {
     console.error('Failed: No audio');
@@ -153,6 +189,10 @@ function trigger() {
   });
 }
 
+/**
+ * Start the event loops. Starts listening and buffering input audio, and
+ * listening to keypresses.
+ */
 function start() {
   startAudioInput();
   process.stdin.on('data', (data) => {
@@ -164,7 +204,6 @@ function start() {
     if (DEBUG === 2) {
       console.log(data.keycode);
     }
-    // console.log(data.keycode);
 
     // PlayPause press
     // if (!data.altKey && data.keycode === 57378 && !data.shiftKey &&
@@ -173,9 +212,11 @@ function start() {
     // if (!data.altKey && data.keycode === 21 && !data.shiftKey &&
     //     !data.ctrlKey && data.metaKey) {
 
+    // F2
     if (!data.altKey && data.keycode === 60 && !data.shiftKey && !data.ctrlKey
         && !data.metaKey) {
       queued = true;
+    // Win + u
     } else if (!data.altKey && data.keycode === 22 && !data.shiftKey &&
         !data.ctrlKey && data.metaKey) {
       if (audioBuffer.length == 0) return;
@@ -186,15 +227,27 @@ function start() {
   iohook.start();
 }
 
+/**
+ * Ensure the cache directory exists.
+ */
 function setupCache() {
   fs.mkdir(cacheDir, {recursive: true}, (err) => {
     if (err && err.code !== 'EEXIST') throw err;
   });
 }
+/**
+ * Start listening to input audio.
+ */
 function startAudioInput() {
   input.on('data', onInputData);
   audioHeartbeat();
 }
+/**
+ * Handle a new audio packet. Pushes it into the buffer, and purges stale
+ * packets.
+ *
+ * @param {Object} data The received audio data.
+ */
 function onInputData(data) {
   audioHeartbeat();
 
@@ -234,6 +287,10 @@ function onInputData(data) {
     }
   }
 }
+/**
+ * Consider the audio input to be dead. This will clear the current audio buffer,
+ * end all audio input processing, and attempt to restart the inputs.
+ */
 function audioInputDead() {
   console.log('Restarting AudioInput');
   audioBuffer.splice(0);
@@ -248,10 +305,15 @@ function audioInputDead() {
   }, 500);
 }
 
+/**
+ * If the heartbeat is not fired for a period of time, it will consider the
+ * audio input to be dead, and call {@link audioInputDead}. This function is no
+ * longer used, and doesn't do anything.
+ */
 function audioHeartbeat() {
-  return;
   // clearTimeout(audioTimeout);
   // audioTimeout = setTimeout(audioInputDead, 2000);
+  return;
 }
 
 setupInput();
